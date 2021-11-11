9e1abfb72b2455d67abb4164a4b9b724bb51e9b0
somerandomstone <weyip001@gmail.com>
2021-11-12 00:20:38 +0800

    test gh actions

--- tests/LoginLogoutTest.php
+++ tests/LoginLogoutTest.php
@@ -1,14 +1,33 @@
 <?php
 
-require __DIR__ . "/../admin/adminLoginCTRL.php";
+// require __DIR__ . "/../admin/adminLoginCTRL.php";
 
 
 class LoginLogoutTest extends \PHPUnit\Framework\TestCase {
 
+    public static function onSubmit($id, $password) 
+    {  
+        $role = "Admin";
+        $conn = mysqli_connect("localhost","root","","csit314"); 
+        $check = mysqli_query($conn, "SELECT * from user where id='$id' and password='$password' and role='$role'");  
+        $data = mysqli_fetch_array($check);  
+        $result = mysqli_num_rows($check);  
+        if ($result == 1) {  
+            $_SESSION['adminSession'] = true;  
+            $_SESSION['id'] = $data['id']; 
+            $_SESSION['password'] = $data['password'];    
+            return true;  
+        } 
+        else 
+        {  
+            return false;  
+        }  
+    }
+
     public function testLoginWithRightDetails(): void{
         $this->assertEquals(
             true,
-            adminLoginCTRL::onSubmit('1003', 'admc')
+            LoginLogoutTest::onSubmit('1003', 'admc')
         );
     }
 
